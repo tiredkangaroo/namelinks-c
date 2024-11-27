dbaa69a12b251ebfbb07d0b8923f4c4a328c91df
Ajitesh Kumar <ajinest6@gmail.com>
2024-11-27 14:56:46 -0500

    added list response

--- main.c
+++ main.c
@@ -7,8 +7,17 @@
 
 #define BUFFER_SIZE 120
 #define PATH_SIZE 24
+
 #define DEFAULT_NAMEDURLS_SIZE 128
-#define RESPONSE_SIZE 256
+
+// REDIRECT_RESPONSE_SIZE specifies the size for a redirect
+// HTTP response. Since the max size for a long URL is 128,
+// the REDIRECT_RESPONSE_SIZE is 232 bytes at the maximum.
+#define REDIRECT_RESPONSE_SIZE 256
+
+// LIST_RESPONSE_SIZE specifies the size for a response to
+// /list. Rough calculations suggest the size specified.
+#define LIST_RESPONSE_SIZE (namedURLS_size * 256) + 512
 
 struct NamedURL {
     char* name;
@@ -24,12 +33,12 @@ const char* NOT_FOUND =  "HTTP/1.1 404 Not Found \r\n"
                     "<pre>There is no registry for the name your provided.</pre>";
 
 char* REDIRECT_RESPONSE(char * to) {
-    char* location = malloc(RESPONSE_SIZE);
+    char* location = malloc(REDIRECT_RESPONSE_SIZE);
     if (location == NULL) {
         return NULL;
     }
     sprintf(location,
-        "HTTP/1.1 308 OK \r\n"
+        "HTTP/1.1 308 Permanent Redirect \r\n"
         "Cache-Control: no-store\r\n"
         "Content-Length: 0\r\n"
         "Location: %s\r\n"
@@ -82,15 +91,43 @@ char* getLongURL(void *namedURLS, int namedURLS_size, char* path) {
     return NULL;
 }
 
+char* LIST_RESPONSE(void *namedURLS, int namedURLS_size){
+    char* response = malloc(LIST_RESPONSE_SIZE);
+    char* body = malloc(namedURLS_size * 256 + 256);
+
+    struct NamedURL namedURL;
+    int i = 0;
+    while (i < namedURLS_size) {
+        int start = i * sizeof(struct NamedURL);
+        memcpy((void *)&namedURL, &namedURLS[start], sizeof(struct NamedURL));
+        sprintf(&body[i * 256], "<li><a href='%s'>%s</a></li>", namedURL.url, namedURL.name); // max size 177 bytes
+        i += 1;
+    }
+
+    sprintf(response,
+        "HTTP/1.1 200 OK \r\n"
+        "Cache-Control: no-store\r\n"
+        "Content-Type: text/html\r\n"
+        "Content-Length: %lu\r\n"
+        "\r\n"
+        "%s",
+        strlen(body), body);
+
+    free(body);
+    return response;
+}
+
 int main() {
     void *namedURLS = malloc(sizeof(struct NamedURL) * DEFAULT_NAMEDURLS_SIZE);
     int namedURLS_size = 0;
 
+    // example for testing:
     struct NamedURL helloWorld;
     helloWorld.name = "/yt";
     helloWorld.url = "https://youtube.com";
     memcpy(namedURLS, &helloWorld, sizeof(struct NamedURL));
     namedURLS_size += 1;
+
 
     // local address (returned after a bind)
     struct sockaddr_in my_addr;
@@ -138,11 +175,25 @@ int main() {
             continue;
         }
 
+        if (strcmp(path, "/list") == 0) {
+            char *response = LIST_RESPONSE(namedURLS, namedURLS_size);
+            if (response != NULL) {
+                if (write(cfd, response, strlen(response)) == -1) {
+                    perror("write to a peer connection for list response failed");
+                }
+                free(response);
+            }
+            free(path);
+            shutdown(cfd, SHUT_WR);
+            close(cfd);
+            continue;
+        }
+
         char* longurl = getLongURL(namedURLS, namedURLS_size, path);
 
         if (longurl == NULL) {
             if (write(cfd, NOT_FOUND, strlen(NOT_FOUND)) == -1) {
-                perror("write to a peer connection failed");
+                perror("write to a peer connection for not found failed");
             }
             free(path);
             shutdown(cfd, SHUT_WR);
